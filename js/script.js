92f04a50826c461f0673b575c5d1550d3e1bde7d
ErikaCetera <ceteraerika@gmail.com>
2024-11-11 17:23:09 +0100

    feat: numeri casuali e conto alla rovescia

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
+const formElem = document.getElementById("form-user")
 const timerElem = document.getElementById("timer");
 const imputElem = document.querySelectorAll("imput");
 const btnElem = document.querySelector(".btn");
+const randomNums = document.getElementById("random-numbs")
+
+
+// Calcolo 5 numeri casuali
+
+const randomNumbers = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+randomNums.innerHTML = randomNumbers(10000, 99999)
+
+//  Conto alla rovescia di 30 secondi
+let counter = 30;
+const timercount = setInterval(function () {
+    console.log(counter);
+    counter--;
+    timerElem.innerHTML = counter;
+    if(counter === 0){
+    randomNums.classList.add("d-none");
+    formElem.classList.remove("d-none");
+    clearInterval(timercount);
+    }
+}, 1000);
+
+const 
 
